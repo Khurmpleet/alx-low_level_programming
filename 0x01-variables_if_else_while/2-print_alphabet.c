6e4923e5f0d411ae3c158fece6fdaa331b3bc2a5
Khurmpleet <96843388+Khurmpleet@users.noreply.github.com>
2022-09-09 12:51:09 -0700

    Print the alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Return: Alwys 0(Success)
+ */
+int main(void)
+{
+	char alpha ;
+	
+	for (alpha = 'a'; c<= 'z'; c++ )
+	{
+		putchar(alpha);
+		putchar('\n');
+	}
+return (0);
+}
