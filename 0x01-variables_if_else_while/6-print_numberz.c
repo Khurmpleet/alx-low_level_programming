c2ebf5fcba3d9db2827969faf83dd6601821ad87
Khurmpleet <96843388+Khurmpleet@users.noreply.github.com>
2022-09-09 13:35:56 -0700

    Printing all single digitsfrom 10 to 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print 0123456789 using putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
