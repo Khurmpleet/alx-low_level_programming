f5501fec4adfc5b0c2fb29007d7ddaf5174fac6f
Khurmpleet <96843388+Khurmpleet@users.noreply.github.com>
2022-09-13 16:23:24 -0700

    commenting old code out

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,24 +5,24 @@
  * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
  * Numbers must be coma and space separated.
  * Return: 0 Always (success)
+ *int main(void)
+ * {
+ * 	int counter;
+ * 	int countto = 50;
+ * 	long a = 1;
+ * 	long b = 2;
+ * 
+ * 	for (counter = 1; counter <= (countto / 2); counter++)
+ * 	{
+ * 		printf("%li, %li, ", a, b);
+ * 		a += b;
+ * 		b += a;
+ * 	}
+ * 	if (countto % 2 == 1)
+ * 		printf("%li ", a);
+ * 
+ * 	printf("\n");
+ * 
+ * 	return (0);
+ * }
  */
-int main(void)
-{
-	int counter;
-	int countto = 50;
-	long a = 1;
-	long b = 2;
-
-	for (counter = 1; counter <= (countto / 2); counter++)
-	{
-		printf("%li, %li, ", a, b);
-		a += b;
-		b += a;
-	}
-	if (countto % 2 == 1)
-		printf("%li ", a);
-
-	printf("\n");
-
-	return (0);
-}
