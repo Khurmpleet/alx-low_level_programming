023ddcf37d447488d91d5a784c90ffc2e1da8377
Khurmpleet <96843388+Khurmpleet@users.noreply.github.com>
2022-09-13 14:36:20 -0700

    Program to print _putchar followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries
+ * Return: 0 on success execution.
+ */
+int main (void)
+{
+	int i = 0;
+	char s[] = "_putchar";
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
