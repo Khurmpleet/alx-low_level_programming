d02cbe894a5f58addf080fff25d198c26b7e841c
Khurmpleet <96843388+Khurmpleet@users.noreply.github.com>
2022-09-19 14:16:43 -0700

    Function to print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+include "main."
+
+/**
+ * print_rev - Prints a string in reverse, then a new line
+ * @s: String to reverse
+ */
+
+void print_rev(char *s)
+{
+	int len;
+	
+	/* Iteration to find the length of string and point to last character */
+	for (len = 0; s[len] != '\0'; len++)
+		;
+	
+	/* Itertion to return to character before null and print the string reversed*/
+	for (len--; len >= 0; len--)
+		_putchar(s[len]);
+
+	_putchar('\n');
+
+}
