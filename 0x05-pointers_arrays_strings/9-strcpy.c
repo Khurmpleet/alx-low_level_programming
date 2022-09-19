1dc2d513d330223c33aa0924acbcc3dc2b6cee55
Khurmpleet <96843388+Khurmpleet@users.noreply.github.com>
2022-09-19 15:40:50 -0700

    Function that copies the string produced to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - Copies the string pointed to by src to given memory
+ * @dest: Copy source to this buffer
+ * @src: This is the source to copy
+ * Return: Copy of Origial source
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	inti;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
