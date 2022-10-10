9b791ff573a8692d481ac2ae78af9336d7ad7671
Khurmpleet <96843388+Khurmpleet@users.noreply.github.com>
2022-10-10 12:52:20 -0700

    Program that prints the name of the file it was compiled from, followed by a new line.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - print the the name of the file it was compiled from, followed by a new line
- *
+ * main - print the the name of the file it was compiled from,
+ * followed by a new line
  * Return: 0 on success
  */
 
