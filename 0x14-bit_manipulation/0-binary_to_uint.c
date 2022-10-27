7e442e7da1b9e6008104f96a22cbbd491c668472
Khurmpleet <96843388+Khurmpleet@users.noreply.github.com>
2022-10-27 15:01:27 -0700

    Function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 #include <stdio.h>
 
 /**
@@ -8,23 +9,32 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total, power;
-	int len;
+
+	int len = 0, i;
+	unsigned int sum = 0;
 
 	if (b == NULL)
-		return (0);
+		return (sum);
 
-	for (len = 0; b[len]; len++)
+	/* find string length */
+	while (b[len] != '\0')
+		len++;
+	len -= 1;
+
+	/* iterate string and if '1' then multiply by power of 2 */
+	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
+	i = 0;
+	while (b[i])
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
+		if ((b[i] != '0') && (b[i] != '1'))
+			return (sum);
+
+		if (b[i] == '1')
+			sum += (1 * (1 << len));
+		i++;
+		len--;
 	}
 
-	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
-	{
-		if (b[len] == '1')
-			total += power;
-	}
-
-	return (total);
+	return (sum);
 }
+
